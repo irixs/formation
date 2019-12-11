bde8147168f823e9ed632e45b4823a81e28496ca
Rebeca Silva <ros4@cin.ufpe.br>
2019-12-10 21:11:37 -0300

    refatoração dos testes de aceitação

--- tests-acceptance/features/musica.feature
+++ tests-acceptance/features/musica.feature
@@ -11,12 +11,12 @@ Then eu posso ver na lista musicas disponiveis a musica com titulo “No Air”,
 
 Scenario: Cadastro de Musica duplicada
 Given eu estou na pagina de cadastro de musica
-Given existe uma música com titulo “Sweet Chaos”, artista “DAY6” e ID "6" na lista de musicas disponiveis
+Given existe uma música com titulo “Sweet Chaos”, artista “DAY6”, integrantes “Sungjin,Young K,Jae,Wonpil,Dowoon” e ID "6" na lista de musicas disponiveis
 When eu tento cadastrar uma musica com titulo “Sweet Chaos”, artista “DAY6”, integrantes “Sungjin,Young K,Jae,Wonpil,Dowoon” e ID "6"
 Then eu nao vejo na lista musicas disponiveis a musica com titulo “Sweet Chaos”, artista “DAY6” e ID "6" duplicada
 
 Scenario: Cadastro de Musica com ID ja existente
 Given eu estou na pagina de cadastro de musica
-Given existe uma música com titulo “Umpah Umpah”, artista “Red Velvet” e ID "5" na lista de musicas disponiveis
+Given existe uma música com titulo “Umpah Umpah”, artista “Red Velvet”, integrantes “Irene,Seulgi,Wendy,Joy,Yeri” e ID "5" na lista de musicas disponiveis
 When eu tento cadastrar uma musica com titulo “Follow”, artista “Monsta X”, integrantes “Shownu,Wonho,Minhyuk,Kihyun,Hyungwon,Joohoney,I.M” e ID "5"
 Then eu nao vejo na lista musicas disponiveis a musica com titulo “Follow”, artista “Monsta X” e ID "5"
--- tests-acceptance/stepdefinitions/musicas.ts
+++ tests-acceptance/stepdefinitions/musicas.ts
@@ -3,11 +3,37 @@ import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
+// FUNCOES
+
 let sameArtista = ((elem, artista) => elem.element(by.name('artistalist')).getText().then(text => text === artista));
 let sameTitulo = ((elem, titulo) => elem.element(by.name('titulolist')).getText().then(text => text === titulo));
 let sameID = ((elem, id) => elem.element(by.name('idlist')).getText().then(text => text === id));
 
 let pAND = ((p,q,r) => p.then(a => q.then(b => r.then(c => (a && b) && c))))
+
+async function cadastrarMusica(titulo, artista, integrantes, id) {
+    await $("input[name='titulobox']").sendKeys(<string> titulo);
+    await $("input[name='artistabox']").sendKeys(<string> artista);
+    await $("input[name='integrantesbox']").sendKeys(<string> integrantes);
+    await $("input[name='idbox']").sendKeys(<string> id);
+    await element(by.buttonText('Adicionar')).click();
+}
+
+// verifica o tamanho do array de acordo com o parametro n
+async function tamanhoIgualA(set,n) {
+    await set.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(n));
+}
+
+// verifica se existe uma musica cadastrada ou nao de acordo com o titulo, artista e id 
+// (0 para saber se não existe, 1 para saber se existe uma e etc)
+async function musicasIguais(n,titulo,artista,id) { 
+    var allmusicas : ElementArrayFinder = element.all(by.name('musicaslist'));
+        var samemusica = allmusicas.filter(elem =>
+            pAND(sameArtista(elem,artista),sameTitulo(elem,titulo),sameID(elem,id)));
+    await tamanhoIgualA(samemusica,n);
+}
+
+// GIVE WHEN E THEN
 
 defineSupportCode(function ({ Given, When, Then }) {
 
@@ -30,47 +56,28 @@ defineSupportCode(function ({ Given, When, Then }) {
     })
 
     Given(/^nao existe uma musica com titulo “([^\"]*)”, artista “([^\"]*)” e ID "(\d*)" na lista de musicas disponiveis$/, async (titulo, artista, id) => {
-        var allmusicas : ElementArrayFinder = element.all(by.name('musicaslist'));
-        var samemusica = allmusicas.filter(elem =>
-            pAND(sameArtista(elem,artista),sameTitulo(elem,titulo),sameID(elem,id)));
-        await samemusica.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+        await musicasIguais(0,titulo,artista,id);
     });
 
     When(/^eu tento cadastrar uma musica com titulo “([^\"]*)”, artista “([^\"]*)”, integrantes “([^\"]*)” e ID "(\d*)"$/, async (titulo, artista, integrantes, id) => {
-        await $("input[name='titulobox']").sendKeys(<string> titulo);
-        await $("input[name='artistabox']").sendKeys(<string> artista);
-        await $("input[name='integrantesbox']").sendKeys(<string> integrantes);
-        await $("input[name='idbox']").sendKeys(<string> id);
-        await element(by.buttonText('Adicionar')).click();
+        await cadastrarMusica(titulo,artista,integrantes,id);
     });
 
     Then(/^eu posso ver na lista musicas disponiveis a musica com titulo “([^\"]*)”, artista “([^\"]*)” e ID "(\d*)"$/, async (titulo, artista, id) => {
-        var allmusicas : ElementArrayFinder = element.all(by.name('musicaslist'));
-        await allmusicas.filter(elem => pAND(sameArtista(elem,artista),sameTitulo(elem,titulo),sameID(elem,id))).then
-                   (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        await musicasIguais(1,titulo,artista,id);
     });
 
-    Given(/^existe uma música com titulo “([^\"]*)”, artista “([^\"]*)” e ID "(\d*)" na lista de musicas disponiveis$/, async (titulo, artista, id) => {
-        await $("input[name='titulobox']").sendKeys(<string> titulo);
-        await $("input[name='artistabox']").sendKeys(<string> artista);
-        await $("input[name='integrantesbox']").sendKeys(<string> "Sungjin,Young K,Jae,Wonpil,Dowoon");
-        await $("input[name='idbox']").sendKeys(<string> id);
-        await element(by.buttonText('Adicionar')).click();
-        var allmusicas : ElementArrayFinder = element.all(by.name('musicaslist'));
-        await allmusicas.filter(elem => pAND(sameArtista(elem,artista),sameTitulo(elem,titulo),sameID(elem,id))).then
-                   (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    Given(/^existe uma música com titulo “([^\"]*)”, artista “([^\"]*)”, integrantes “([^\"]*)” e ID "(\d*)" na lista de musicas disponiveis$/, async (titulo, artista, integrantes, id) => {
+        await cadastrarMusica(titulo,artista,integrantes,id);
+        musicasIguais(1,titulo,artista,id);
     });
 
     Then(/^eu nao vejo na lista musicas disponiveis a musica com titulo “([^\"]*)”, artista “([^\"]*)” e ID "(\d*)" duplicada$/, async (titulo, artista, id) => {
-        var allmusicas : ElementArrayFinder = element.all(by.name('musicaslist'));
-        await allmusicas.filter(elem => pAND(sameArtista(elem,artista),sameTitulo(elem,titulo),sameID(elem,id))).then
-                   (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        musicasIguais(1,titulo,artista,id);
     });
 
     Then(/^eu nao vejo na lista musicas disponiveis a musica com titulo “([^\"]*)”, artista “([^\"]*)” e ID "(\d*)"$/, async (titulo, artista, id) => {
-        var allmusicas : ElementArrayFinder = element.all(by.name('musicaslist'));
-        await allmusicas.filter(elem => pAND(sameArtista(elem,artista),sameTitulo(elem,titulo),sameID(elem,id))).then
-                   (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+        musicasIguais(0,titulo,artista,id);
     });
 
 })
